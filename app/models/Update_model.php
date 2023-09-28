711c3f63694a99e86a4cce75ec549437ddaf6b23
sariaRiski02 <muhammatsaria026@student.unsrat.ac.id>
2023-09-28 11:50:43 +0700

    make logic edit for all input in page edit

--- app/models/Update_model.php
+++ app/models/Update_model.php
@@ -25,42 +25,56 @@ class Update_model
         return (in_array($typefile, $typeImg)) ? true : false;
     }
 
+
     // upload to directory
-
-
     public function Update($id, $data)
     {
-        if (isset($_POST["update"])) {
-            $cover = ($_FILES["change_cover"]["name"]);
+        if (isset($data["update"])) {
+            $cover = $_FILES["change_cover"]["name"];
             $title = htmlspecialchars($data["change_title"]);
             $author = htmlspecialchars($data["change_author"]);
             $sinopsis = htmlspecialchars($data["change_sinop"]);
 
+            $exist = isset($cover) && isset($title) && isset($author) && isset($sinopsis);
+            if (!$exist) {
+                return $this->message("failed", "Lengkapi data");
+                exit;
+            }
+
+            // CHANGE COVER
             if (!$this->checkImg($cover)) {
                 $message = "Sampul hanya dapat berformat PNG, JPEG, JPG, atau GIF";
                 return $this->message("failed", $message);
             }
 
             $max_size_img = 2000000;
-            if (filesize($_FILES["change"]["tmp_name"]) > $max_size_img) {
+            if (filesize($_FILES["change_cover"]["tmp_name"]) > $max_size_img) {
                 $message = "Max 2MB";
                 return $this->message("failed", $message);
                 exit;
             }
 
+
+            $this->db->query("UPDATE " . $this->table_name . " SET cover=:cover" . " WHERE id=:id");
+            $this->db->bind("id", $id);
+            $this->db->bind("cover", $cover);
+            $this->db->execute();
+            if ($this->db->rowCount() < 1) {
+                $message = "Kesalahan System, Mohon Coba lagi! 1";
+                return $this->message("failed", $message);
+                exit;
+            }
+
             // move to directory
-            $target_dir = "../app/assets/cover/" . $cover;
-
+            $target_dir = "../public/assets/cover/" . $cover;
             $result = (move_uploaded_file($_FILES["change_cover"]["tmp_name"], $target_dir));
-
             if (!$result) {
                 $message = "cover tidak bisa diunggah, Coba Lagi!";
                 return $this->message("failed", $message);
                 exit;
             }
-
             // get the name of file img in the database to delete from directory
-            $this->db->query("SELECT cover FROM $this->table_name WHERE id=:id ");
+            $this->db->query("SELECT cover FROM $this->table_name WHERE id=:id");
             $this->db->bind("id", $id);
             $old_cover = $this->db->single()["cover"];
             if ($this->db->rowCount() < 1) {
@@ -70,15 +84,27 @@ class Update_model
             }
             unlink("../public/assets/cover/" . $old_cover);
 
-
-            // get the name of file sinops in the database to delete from directory
-
-
-
+            // CHANGE TITLE & 
 
 
             // update database
-            $this->db->query("UPDATE $this->table_name SET cover=:cover, author=:author, title=:title, sinopsis=:sinopsis");
+            $this->db->query("UPDATE $this->table_name SET author=:author, title=:title WHERE id=:id");
+            $this->db->bind("author", $author);
+            $this->db->bind("title", $title);
+            $this->db->bind("id", $id);
+            $this->db->execute();
+            if ($this->db->rowCount() < 1) {
+                $this->message("failed", "perubahan gagal dilakukan");
+            }
+
+            // get the name of file sinops in the database to delete from directory
+            $this->db->query("SELECT sinopsis FROM " . $this->table_name . " WHERE id=:id");
+            $this->db->bind("id", $id);
+            $file_name_sinop = $this->db->single()["sinopsis"];
+            $myfile = fopen("../app/assets/desc/" . $file_name_sinop, "w") or die("tidak bisa mengubah sinopsis");
+            fwrite($myfile, $sinopsis);
+            fclose($myfile);
+            return $this->message("success", "berhasil diubah");
         }
         return $this->message("", "");
         exit;
--- app/views/Update/index.php
+++ app/views/Update/index.php
@@ -3,7 +3,7 @@
         <div class="cover">
             <img src="<?= BASEURL; ?>/assets/cover/<?= $data["item"]["cover"] ?>" width="150px" alt="cover" id="photoPreview">
         </div>
-        <form method="post">
+        <form method="post" enctype="multipart/form-data">
             <div class="input-cover">
                 <input type="file" accept="image/*" name="change_cover" id="change_cover" style="display: none;">
 
@@ -14,7 +14,7 @@
                 <input type="text" name="change_author" require placeholder="Penulis : <?= $data["item"]["author"] ?>">
             </div>
             <textarea name="change_sinop" cols="30" rows="10" placeholder="Sinopsis" id="textarea"><?= $data["item"]["sinopsis"] ?></textarea>
-            <div class="success">berhasil</div>
+            <div class="<?= $data["warning"]["class"] ?>"><?= $data["warning"]["message"] ?></div>
             <button type="submit" name="update">Ubah</button>
         </form>
     </div>
